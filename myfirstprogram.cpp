b9d388205c6c2f2570a544ae573e165518e67f80
Muhammad Romadhoni A. <romadhonim1@gmail.com>
2025-09-21 23:17:24 +0700

    hahaha

---
+++ myfirstprogram.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout << "Hello World!\n";
+ cout << "Have a good day!" << endl;
+ cout << "end the C++ program successfully\n\n";
+ cout << "I am learning c++\\";
+ cout << "They call him \"Johnny\"." <<"\n"; //bisa disini juga
+ //ini agar tidak muncul di output tapi bisa tambahan keterangan
+ /* coba yang ini juga*/
+ int Nominal = 15;
+ cout << "\n" << Nominal;
+// jarak
+ int myNum = 15;
+ cout << myNum << endl;
+ //jarak
+ int mYNum;
+ mYNum = 15;
+ cout << mYNum;
+ //jeda
+ int mYNUm = 15;
+ mYNUm = 20;
+ cout << mYNUm;
+ //Variables
+ int MYNUM = 5;
+ double myFloatNum = 5.99;
+ char myletter = 'D';
+ string myText = "Hello";
+ bool myBoolean = true;
+ // semua variabel bisa di cout
+ cout << endl; //enter
+ int myAge = 35;
+ cout << "I am " << myAge << " years old." <<endl;
+ int x = 5;
+ int y = 6;
+ int tambah = x + y;
+ cout << tambah <<endl;
+ const int minutesPerHour = 60;
+ const float PI = 3.14;
+ cout << PI << endl;
+ cout << minutesPerHour << endl;
+ //data murid
+ int id_murid = 15;
+ int studentAge = 18;
+ float studentFee = 75.25;
+ char studentGrade = 'B';
+ //print data
+ cout << "Student Id: " << id_murid << endl;
+ cout << "Student Age : " << studentAge << endl;
+ cout << "Student Fee : " << studentFee << endl;
+ cout << "Student Grade : " << studentGrade << endl;
+
+ int x;
+ cout << "Masukkan Nomor Telepon : ";
+ cin >> x;
+ cout << "Nomormu adalah :" << x;
+
+
+
+ return 0;
+}
